d148f81e6c8ea7e5b527ea0f35f9a94effdf4d53
Andrey Vasilyev <avasilyev475@gmail.com>
2022-04-08 00:49:44 -0400

    End Of Section 6, Lesson 73 - Section 6 Summary

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,29 +1,41 @@
+//Angular imports:
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { NavComponent } from './nav/nav.component';
 import { FormsModule } from '@angular/forms';
+
+//All other imports:
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { SharedModule } from './_modules/shared.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    MemberListComponent,
+    MemberDetailComponent,
+    ListsComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    BsDropdownModule.forRoot(),
-    FormsModule
+    FormsModule, 
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
